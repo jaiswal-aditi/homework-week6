e41e72fa24f29f81f2bb46010aa588835902e83e
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-02-23 13:24:10 -1000

    Update delete_element.c

--- delete_element.c
+++ delete_element.c
@@ -10,10 +10,12 @@ Note:
     - position from where an element needs to be deleted
 2. Upon deletion of the element at the specified position, all elements after that position must be shifted one position to the left to fill up the gap.
 3. Display output as:
-    Array after deletion: <array elements separated by a single space>
+    The new array is: <array elements separated by a single space>
+4. Use the input/output prompts as suggested in the pdf.
+5. Remember that array index starts from 0.
 
 Example:
-Original array: 6 7 8 9 10
+If the original array is -> 6 7 8 9 10
 --> delete element at position 2 (element: 8)
-Array after deletion: 6 7 9 10
+The new array is: 6 7 9 10
 */
